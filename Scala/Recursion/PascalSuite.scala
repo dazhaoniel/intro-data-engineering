c6f027cb2c519a8a2c12937af9ad52c6c4dbbead
zhaoj <daniel.zhao@dowjones.com>
2016-10-16 23:14:38 -0400

    add first assignment on recursion

---
+++ Scala/Recursion/PascalSuite.scala
@@ -0,0 +1,33 @@
+package recfun
+
+import org.scalatest.FunSuite
+
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class PascalSuite extends FunSuite {
+  import Main.pascal
+    test("pascal: col=0,row=2") {
+      assert(pascal(0,2) === 1)
+  }
+
+    test("pascal: col=1,row=2") {
+      assert(pascal(1,2) === 2)
+  }
+
+    test("pascal: col=1,row=3") {
+      assert(pascal(1,3) === 3)
+  }
+
+    test("pascal: col=100,row=4") {
+      assert(pascal(100,4) === 0)
+  }
+
+    test("pascal: col=4,row=100") {
+      assert(pascal(4,100) === 3921225)
+  }
+
+
+}
